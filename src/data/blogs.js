3fa16e03a9783be84bd531cd2bb17c5ed78b61d4
David Salathé <salathe.david@gmail.com>
2025-12-25 20:25:16 +0000

    Github Pages redirect to blog.dsalathe.dev

--- src/data/blogs.js
+++
@@ -1,9 +0,0 @@
-import { loadBlogPosts, loadBlogPost } from '../utils/blogLoader';
-
-export const getBlogs = async (includeAllPosts = false) => {
-  return await loadBlogPosts(includeAllPosts);
-};
-
-export const getBlogById = async (id, ignorePublishDate = false) => {
-  return await loadBlogPost(id, ignorePublishDate);
-};
